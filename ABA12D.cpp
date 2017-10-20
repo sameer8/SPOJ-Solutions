fee07d76e5ba106f222650e3f36f9347905f9d48
nitikataneja <way2nitika@gmail.com>
2017-10-20 18:40:10 +0530

    ABA12D added

---
+++ ABA12D.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool prime[1000005];
+void precompute(){
+	int n=1000000;
+	memset(prime,true,sizeof(prime));
+	prime[1]=false;
+	prime[0]=false;
+	for(int i=2;i*i<=n;i++){
+		if(prime[i]){
+			for(int j=i*i;j<=n;j+=i)
+			prime[j]=false;
+		}
+	}
+}
+
+ main(){
+ 	int t;
+ 	scanf("%d",&t);
+ 	while(t--){
+ 		int a,b;
+ 		scanf("%d%d",&a,&b);
+ 		int c=0;
+ 		for(int i=a;i<=b;i++){
+ 			if(prime[i])c++;
+ 			else if(i>2 and prime[i-2])c++;
+		 }
+		 printf("%d\n",c);
+	 }
+ }
