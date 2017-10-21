b33278b5d4b0d3b96e56c2496dcbce154ffcaad0
Salonitaneja <way2salonitaneja@gmail.com>
2017-10-21 20:29:57 +0530

    TSHOW1 added

---
+++ TSHOW1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		long long int n;
+		cin>>n;
+		long long int temp,num,mul,len;
+		mul=2;
+		len=1;
+		while(n>mul){
+			len+=1;
+			n-=mul;
+			mul*=2;
+		}
+		n=n-1;
+		//cout<<n<<endl;
+		string s="56";
+		string tem="";
+		while(len--){
+			temp=n%2;
+			tem=s[temp]+tem;
+			n/=2;
+		}
+		cout<<tem<<endl;
+	}
+}
